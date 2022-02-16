e823934fe8d4a064a7122963ea5e4ec1fe026409
Nitya <gorenityanand96@gmail.com>
2022-02-16 13:50:36 +0530

    [Nityanand]: Single Delegation

---
+++ Operations.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace DelegatesDemo
+{
+    public class Operations
+    {
+        public static void Add(int a, int b)
+        {
+            System.Console.WriteLine("Addtion of a+b = "+(a+b));
+        }
+        public static void Multiplication(int a, int b)
+        {
+            System.Console.WriteLine("Multiplication of a*b  = "+(a*b));
+        }
+    }
+}
